02c4821dfbe872f7e6e561d8ced21a4a10ee436c
vadimsoude <vadimsoude@gmail.com>
2023-11-09 19:01:51 +0100

    Fix entity rotation bug !!

--- core/src/fr/soudepriezleroux/entity/Entity.java
+++ core/src/fr/soudepriezleroux/entity/Entity.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.TimeUtils;
 
@@ -14,7 +13,6 @@ import java.util.UUID;
 public class Entity {
     private final UUID uuid;
     private final ArrayList<Sprite> textures;
-    private SpriteBatch sprite;
     private final Rectangle hitbox;
     private float[] screenCoord;
     private final float[] textureSize;
@@ -27,7 +25,6 @@ public class Entity {
     public Entity(String prefix, boolean isAnimated, int nbrFrame, float width, float height, float x, float y, float textureSizeX, float textureSizeY, Facing facing){
 
         this.uuid = UUID.randomUUID();
-        sprite = new SpriteBatch();
 
         this.facing = facing;
 
@@ -39,18 +36,15 @@ public class Entity {
 
         for (int i = 0; i < nbrFrame; i++) {
             Sprite spriteTemp = new Sprite(new Texture(Gdx.files.internal(prefix + "_f_"+i+".png")),0,0,16,16);
-            spriteTemp.setRotation(0f);
+            spriteTemp.setRotation(facing.get());
             textures.add(spriteTemp);
         }
     }
 
-    public void render(Matrix4 projection){
+    public void render(SpriteBatch spriteBatch){
 
         hitbox.x = screenCoord[0];
         hitbox.y = screenCoord[1];
-
-        sprite.setProjectionMatrix(projection);
-        sprite.begin();
 
         if(isAnimated){
             if(TimeUtils.millis() > animationLoopTimer + animationTime){
@@ -62,15 +56,15 @@ public class Entity {
                 animationLoopTimer = TimeUtils.millis();
             }
         }
+
         Sprite spriteTemp = textures.get(animationFrame);
+        spriteTemp.setBounds(hitbox.x, hitbox.y,textureSize[0],textureSize[1]);
+        spriteTemp.setOriginCenter();
         spriteTemp.setRotation(facing.get());
-        sprite.draw(spriteTemp, hitbox.x, hitbox.y,textureSize[0],textureSize[1]);
-        sprite.end();
+        spriteTemp.draw(spriteBatch);
     }
 
     public void dispose(){
-        sprite.dispose();
-
         for (Sprite sprite:textures) {
             sprite.getTexture().dispose();
         }
@@ -80,6 +74,7 @@ public class Entity {
     public UUID getUuid() {
         return uuid;
     }
+
     public float[] getScreenCoord() {
         return screenCoord;
     }
