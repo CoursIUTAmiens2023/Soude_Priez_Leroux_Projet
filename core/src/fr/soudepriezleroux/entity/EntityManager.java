78fdb84462f512a2c39e200b535268b70942421d
『 INSØMN!AK 』 <lerouxmatteo1103@gmail.com>
2023-11-15 18:57:56 +0100

    erreurs

--- core/src/fr/soudepriezleroux/entity/EntityManager.java
+++ core/src/fr/soudepriezleroux/entity/EntityManager.java
@@ -8,23 +8,26 @@ import java.util.List;
 import java.util.UUID;
 
 public class EntityManager {
-    private static ArrayList<Entity> entities = new ArrayList<>();
-    private static SpriteBatch spriteBatch = new SpriteBatch();
+    private static ArrayList<Entity> entities;
+    private static SpriteBatch spriteBatch;
 
     public static void init(Camera camera){
+        spriteBatch = new SpriteBatch();
         entities = new ArrayList<>();
         spriteBatch.setProjectionMatrix(camera.combined);
     }
 
-    public static UUID addEntity(Object entity){
-        entities.add((Entity) entity);
-        return ((Entity) entity).getUuid();
+    public static UUID addEntity(Entity entity){
+        entities.add(entity);
+        return (entity).getUuid();
     }
 
-    public static void addEntities(List<?> entities){
-        for (Object entity:entities) {
-            addEntity((Entity) entity);
+    public static List<UUID> addEntities(List<Entity> entities){
+        List<UUID> NewUuids = new ArrayList<>();
+        for (Entity entity:entities) {
+            NewUuids.add(addEntity(entity));
         }
+        return NewUuids;
     }
 
     public static ArrayList<Entity> getEntities(){
